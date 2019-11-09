acb5028088cdeecbae9da09a31c15daedb1c4c1f
PretaxEnd9716 <42460087+PretaxEnd9716@users.noreply.github.com>
2019-11-09 16:40:10 -0600

    main
    this is the final time

---
+++ main.py
@@ -0,0 +1,29 @@
+import praw
+import pyttsx3
+
+
+def get_link():
+    print('hello0')
+
+
+def get_data():
+    print('hello')
+
+
+def get_comments():
+    print('hello2')
+
+
+def master_string():
+    print('hello3')
+
+
+def say_post():
+    print('hello4')
+
+
+def main():
+    print('hello5')
+
+
+main()
