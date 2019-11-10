18a95c1cc6392cd0a808ed258053154d6c32f0ba
ChaseThePan <57573331+ChaseThePan@users.noreply.github.com>
2019-11-10 10:36:35 -0600

    Update master_string
    makes it so it says not available if there is no body

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ def get_data():
 def get_comments():
     print('hello2')
 
-
-def master_string():
-    print('hello3')
+def master_string(title, body, author, comments):
+    if body == '':
+        body = 'Not Available'
+    string = f"TITLE OF POST: {title}\n AUTHOR: {author}\nBODY: {body}\nCOMMENTS: {comments}"
+    return string
 
 
 def say_post():
